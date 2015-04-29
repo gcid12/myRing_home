0b5443e619bcc4c54ac4d13d78f90b1ba177e90e
rcid@myring.io <you@example.com>
2015-04-29 14:20:30 -0400

    Delete /venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gcid/Sites/rings/ring1/venv"
+set -gx VIRTUAL_ENV "/var/www/ringhome/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
